3957db145894c984ba9c58cf563462ce4f87f0ec
LeoRobert <robert.herligton@gmail.com>
2016-10-06 00:53:08 -0400

    Update app.js

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,7 +1,7 @@
 (function () {
 'use strict';
 
-angular.module('myLunch', [])
+angular.module('myLunch', [$scope])
 .controller('LunchController', LunchController);
 
 LunchController.$inject = [$scope];
